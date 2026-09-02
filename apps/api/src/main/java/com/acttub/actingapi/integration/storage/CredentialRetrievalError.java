176d21d2a229ae4b0eb9eb06e4fbe04e3ff01344
Fiddich <78968690+Fiddich-Dev@users.noreply.github.com>
2026-09-03 05:19:51 +0900

    chore(api): 스모크 컨트롤러와 죽은 예외를 지운다

--- apps/api/src/main/java/com/acttub/actingapi/integration/storage/CredentialRetrievalError.java
+++
@@ -1,2 +0,0 @@
-package com.acttub.actingapi.integration.storage;
-public class CredentialRetrievalError extends NoCredentialsError {public CredentialRetrievalError(String message){super(message);}}
